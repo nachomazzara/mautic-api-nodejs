c6036231ba74d497f4b4f023bd7da5c2bd6afcd1
nachomazzara <imazzara@Ignacios-MacBook-Pro.local>
2016-11-27 00:16:05 -0300

    ugly but works

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 'use strict';
 
 module.exports = {
-	BASE_URL: "https://mautic.acadevor.com/",
-  	MAUTIC_PUBLIC_KEY: '16276sv8jchwk8004g4g4so0k8swwwwow0c0sk08s8w0gg4k44',
-  	MAUTIC_SECRET_KEY: '5bpag3cr3w0884soo4k40cgw00skgkow4sc00kkg4owoowowgo'
+	
 };
--- src/index.js
+++ src/index.js
@@ -2,89 +2,179 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import conf from '../config.js';
-import OAuth from 'oauth';
+import {ENV, URLS} from '../src/config/constants';
+import simpleOauth2 from 'simple-oauth2';
 import request from 'request';
- 
+import rp from 'request-promise';
+import axios from 'axios';
+
 let app = express(); // define our app using express
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-let port = process.env.PORT || 3000;  
+let port = process.env.PORT || 3000;
 
-let router = express.Router();              // get an instance of the express Router
+let router = express.Router(); // get an instance of the express Router
 
 router.use(function(req, res, next) {
     console.log('Something is happening.');
     next();
 });
 
-router.get('/', function(req, res) {
-    res.json({ message: 'hey! welcome to mautic api' });   
+// Set the configuration settings
+const credentials = {
+  client: {
+    id: ENV.MAUTIC_PUBLIC_KEY,
+    secret: ENV.MAUTIC_SECRET_KEY,
+  },
+  auth: {
+    tokenHost: ENV.MAUTIC_BASE_URL,
+    tokenPath: ENV.MAUTIC_TOKEN_PATH,
+    authorizePath: ENV.MAUTIC_AUTH_PATH,
+ },
+};
+
+// Initialize the OAuth2 Library
+const oauth2 = simpleOauth2.create(credentials);
+const authorizationUri = oauth2.authorizationCode.authorizeURL({
+  redirect_uri: ENV.BASE_URL + 'callback',
+  state: '4c4d3v0r'
 });
 
-/*request.post({
-    url: conf.BASE_URL + "oauth/v1/request_token",
-    json: {
-    	oauth_consumer_key="CONSUMER_KEY",
-		oauth_nonce="UNIQUE_STRING",
-		oauth_signature="GENERATED_REQUEST_SIGNATURE",
-		oauth_signature_method="HMAC-SHA1",
-		oauth_timestamp="1318467427",
-		oauth_version="1.0"
-    }    
-	
-
-}, function (err, httpResponse, body) {
-    console.log('JSON response from the server: ' + body);
-});*/
-
-let oauth = new OAuth.OAuth(
-      'https://mautic.acadevor.com/oauth/v1/request_token',
-      'https://mautic.acadevor.com/oauth/v1/access_token',
-      conf.MAUTIC_PUBLIC_KEY,
-      conf.MAUTIC_SECRET_KEY,
-      '1.0A',
-      'localhost:3000',
-      'HMAC-SHA1'
-    );
-console.log(oauth);
-oauth.getOAuthRequestToken(function(err, token, token_secret, parsedQueryString){
-	console.log(err);
-	console.log(token);
-	console.log(token_secret);
-	console.log(parsedQueryString);
+// Initial page redirecting to Github
+router.get('/auth', (req, res) => {
+  debugger;
+  console.log(authorizationUri);
+  res.redirect(authorizationUri);
 });
- /*   oauth.get(
-      'https://api.twitter.com/1.1/trends/place.json?id=23424977',
-      'your user token for this app', 
-      //you can get it at dev.twitter.com for your own apps
-      'your user secret for this app', 
-      //you can get it at dev.twitter.com for your own apps
-      function (e, data, res){
-        if (e) console.error(e);        
-        console.log(require('util').inspect(data));
-        done();      
-      });    
-});*/
 
-router.route('/contact')
-	.post(function(req, res) {    
-	    let contact = new Contact();
-	    contact.email = req.body.email;
+// Callback service parsing the authorization token and asking for the access token
+router.get('/callback', (req, res) => {
+  const code = req.query.code;
+  const options = {
+    code,
+    redirect_uri: ENV.BASE_URL + 'callback',
+  };
 
-	    /* save the bear and check for errors
-	    bear.save(function(err) {
-	        if (err)
-	            res.send(err);
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    if (error) {
+      console.error('Access Token Error', error.message);
+      return res.json('Authentication failed');
+    }
 
-	        res.json({ message: 'Contact created!' });
-    	});*/
-    
-	});
+    console.log('The resulting token: ', result);
+    const token = oauth2.accessToken.create(result);
+
+    return res
+      .status(200)
+      .json(token);
+  });
+});
+
+/* API START */
+
+/* /Contacts */
+router.route('/contacts')
+  /* GET /contacts listing. */
+  .get((req,res) => {
+    axios.get(URLS.contacts,{
+        params: {
+          access_token: ENV.ACCESS_TOKEN,
+        }
+      })
+      .then(response => {
+        return response.data
+      })
+      .then(contacts => {
+        res
+        .status(200)
+        .send(contacts);
+      })
+      .catch(err => {
+          res.send(err);
+      });
+  })
+  /* POST /contacts create. */
+	.post((req, res) => {
+
+    if(!req.body.email){
+      return res
+        .status(400)
+        .json({
+          message: 'Bad Request' ,
+          desc:  'Email Needed',
+        });
+    }
+
+    axios.post(URLS.contacts + '/new?access_token=' +  ENV.ACCESS_TOKEN , {
+          email: req.body.email,
+          firstname: req.body.firstname,
+      })
+      .then(response => {
+        console.log(response);
+        return res
+        .status(201)
+        .json({ message: 'Contact created!' });
+      })
+      .catch(err => {
+        console.log(err);
+        return err.response;
+      }).then(response => {
+        return res
+        .status(response.status)
+        .json(response.data);
+      })
+    })
+    /* PUT /contacts edit or create if not exists. */
+    .patch((req, res) => {
+      if(!req.body.email){
+        return res
+          .status(400)
+          .json({
+            message: 'Bad Request' ,
+            desc:  'Email Needed',
+          });
+      }
+
+      let contactID = "";
+
+      axios.get(URLS.contacts,{
+          params: {
+            access_token: ENV.ACCESS_TOKEN,
+            search: 'email:' + req.body.email,
+          }
+        })
+        .then(response => {
+          return response.data.contacts;
+        })
+        .then(contacts => {
+          contactID = contacts[0].id;
+          axios.patch(URLS.contacts + '/' + contactID + '/edit?access_token=' +  ENV.ACCESS_TOKEN , {
+                firstname: req.body.firstname
+            })
+            .then(response => {
+              console.log(response);
+              return res
+              .status(201)
+              .json({ message: 'Contact edited!' });
+            })
+            .catch(err => {
+              console.log(err);
+              return err.response;
+            }).then(response => {
+              res
+              .status(response.status)
+              .json(response.data);
+            })
+        })
+        .catch(err => {
+            res.send(err);
+        });
+
+    });
 
 app.use('/api', router);
 
 app.listen(port)
-console.log('Magic happens on port ' + port);
+console.log('Listening to port ' + port);
